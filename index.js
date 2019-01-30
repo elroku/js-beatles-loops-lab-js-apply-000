0d3483699abacec8c90d678c2f86a8cadd85d356
Desalegn Berihun <desunew@gmail.com>
2019-01-30 01:25:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,13 @@ function johnLennonFacts (facts){
   return newFacts;
 }
 
-function iLoveTheBeatles(Number) {
-  
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return love;
 }
 
