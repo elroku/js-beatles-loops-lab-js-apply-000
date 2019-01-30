c457221b456b51af446d1012dea30042408c4cc8
Desalegn Berihun <desunew@gmail.com>
2019-01-30 02:00:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ function theBeatlesPlay(musicians, instruments) {
   return players
 }
 
-function johnLennonFacts (facts){
-  var facts = [ 'He was the last Beatle to learn to drive!!!', 'He was never a vegetarian!!!', 'He was a choir boy and boy scout!!!', 'He hated the sound of his own voice!!!' ]
- 
-  var i;
-  var newFacts = [];
-  while (i < facts.length){
-    newFacts.push(facts[i]);
-    i++
-  }
-  return newFacts;
+function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
 }
 
 function iLoveTheBeatles(num) {
